2a4f0dd0e2f9fe13872f109811e7bb4c14049c38
Andrew Jones <andrewjones@WTA-MBP522.local>
2020-07-10 18:40:33 -0700

    Allow localhost origin and parse products from server

--- api/server.js
+++ api/server.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const cors = require('cors');
 const products = require('../src/products.json');
 
 const app = express();
 const port = process.env.PORT || 9001;
 
-app.get('/products', (req, res) => {
-  return res.json(products);
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
+app.get('/products', cors(corsOptions), (req, res) => {
+  return res.json(
+    products
+      .filter((product) => product.isVisible)
+      .map((product) => ({
+        categories: product.categories.map((category) => category.name),
+        description: product.description,
+        imageFilename: product.imageFilename,
+        merchRank: product.merchRank,
+        name: product.name,
+        price: product.price,
+        productId: product.productId,
+        productName: product.productName,
+        retailPrice: product.retailPrice,
+        tags: product.tags.reduce(
+          (tagsObj, tag) => ({
+            ...tagsObj,
+            [tag.name]: true,
+          }),
+          {}
+        ),
+        variantId: product.variantId,
+      }))
+  );
 });
 
 app.listen(port, () => console.info(`App started, listening on ${port}`));

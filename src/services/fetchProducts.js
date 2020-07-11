34161338f6d7d3dc942e59ad5d09d1b81c3cbdf2
Andrew Jones <andrewjones@WTA-MBP522.local>
2020-07-10 23:10:08 -0700

    Adds service for calling API

---
+++ src/services/fetchProducts.js
@@ -0,0 +1,35 @@
+// @flow
+
+import axios from 'axios';
+import { PRODUCTS_API } from './routes';
+
+export type Product = {
+  categories: string[],
+  description: string,
+  imageFilename: string,
+  name: string,
+  packageUnitAmount: ?number,
+  packageUnitFormatted: ?number,
+  price: number,
+  productName: string,
+  retailPrice: ?number,
+  tags: { [string]: boolean },
+  variantId: string,
+};
+
+export type ProductsData = {
+  products: Product[],
+  tags: string[],
+  categories: string[],
+};
+
+/**
+ * Fetches all currently available products
+ * @returns {ProductsData}
+ */
+const fetchProducts = async (): Promise<ProductsData> => {
+  const response = await axios.get(PRODUCTS_API);
+  return response.data;
+};
+
+export default fetchProducts;
---
+++ src/services/routes.js
@@ -0,0 +1,5 @@
+// @flow
+
+const BASE_URL = 'http://localhost:9001';
+
+export const PRODUCTS_API = `${BASE_URL}/products`;
